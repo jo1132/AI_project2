84b20f4c163a25f46c421f6e45eb063a497fd99c
jo1132 <jhk1132@gmail.com>
2023-09-20 07:51:59 +0000

    add score demo

--- score.py
+++ score.py
@@ -10,19 +10,19 @@ def parse_args():
     parser = argparse.ArgumentParser(description='get arguments')
 
     parser.add_argument(
-        '--model_path',
-        default='/root/scikit_learn_data/test/labels.csv',
-        type=str,
-        required=False,
-        help='predict model path'
-    )
-
-    parser.add_argument(
         '--label_path',
         default='/root/scikit_learn_data/test/labels.csv',
         type=str,
         required=False,
         help='scoring label path'
+    )
+
+    parser.add_argument(
+        '--pred_path',
+        default='/root/scikit_learn_data/test/labels.csv',
+        type=str,
+        required=False,
+        help='predicted label path'
     )
 
     parser.add_argument(
@@ -33,6 +33,15 @@ def parse_args():
         help='predict as random',
     )
 
+    parser.add_argument(
+        '--demo',
+        default=False,
+        type=bool,
+        required=False,
+        help='For Demo',
+    )
+
+
     args = parser.parse_args()
     return args
 
@@ -40,11 +49,20 @@ def parse_args():
 args = parse_args()
 label_path = args.label_path
 labels = pd.read_csv(label_path)
+labels = labels.loc[:, '1']
 
 if args.random:
     num = [i for i in range(10)]
     pred = [random.choice(num) for _ in range(len(labels))]
 
+elif (args.demo):
+    pred_path = args.pred_path
+    pred = pd.read_csv(pred_path)
+    pred = pred.loc[:, ['1']]
+
+else:
+    pred_path = args.pred_path
+    pred = pd.read_csv(pred_path)
 
 acc = accuracy_score(pred, labels)
 print('accuracy:', acc)
